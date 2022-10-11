966f3d913fba8a58f11794642e28b2119aaad8e6
usrrt <gkenfktm@gmail.com>
2022-10-11 10:43:07 +0900

    [Feat] 캐릭터 이동 구현(마우스 우클릭)

--- AOS Character R&D/Assets/Scripts/Player/PlayerMovement.cs
+++ AOS Character R&D/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     //             MAIL : gkenfktm@gmail.com         
     // ###############################################
 
-    float RotateSpeed = 0.075f;
+    [SerializeField] float RotateSpeed = 0.5f;
     float RotateVelocity;
 
     NavMeshAgent _agent;
@@ -28,10 +28,10 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         // 우클릭시 이동
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButton(1))
         {
             RaycastHit hit;
-
+            
             // 마우스위치에서 쏜 raycast가 물체에 맞는다면, 그곳이 navmesh도착지점
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
@@ -41,11 +41,13 @@ public class PlayerMovement : MonoBehaviour
 
                 // ROTATION
                 Quaternion lookAtTheTarget = Quaternion.LookRotation(hit.point - transform.position);
-                // ???
+                // 캐릭터가 타겟을 향해 회전하는 속도 보간
+                // RotateSpeed값이 작을수록 빠르게 회전함
                 float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, lookAtTheTarget.eulerAngles.y, ref RotateVelocity, RotateSpeed * (Time.deltaTime * 5));
 
                 transform.eulerAngles = new Vector3(0, rotationY, 0);
-                
+
+
             }
         }
     }
